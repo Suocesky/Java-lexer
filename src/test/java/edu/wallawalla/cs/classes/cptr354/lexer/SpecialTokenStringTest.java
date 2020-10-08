63bfd835304130c4380477cf464de7fbf5af9eca
Preston Carman <prestonc@apache.org>
2020-10-08 12:26:03 -0700

    Updating test to use common function.

--- src/test/java/edu/wallawalla/cs/classes/cptr354/lexer/SpecialTokenStringTest.java
+++ src/test/java/edu/wallawalla/cs/classes/cptr354/lexer/SpecialTokenStringTest.java
@@ -1,62 +1,49 @@
 package edu.wallawalla.cs.classes.cptr354.lexer;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 import edu.ucsb.cseweb.classes.cse131a.lexer.Sym;
-import edu.ucsb.cseweb.classes.cse131a.lexer.Token;
-import org.junit.Test;
 
 public class SpecialTokenStringTest {
 
-  private Token token;
+	@Test
+	public void testGetTokenStringTooLong() {
+		String input = "'This test string is much, much longer that 80 characters. Would you not agree? I would.';";
+		String expected = "This test string is much, much longer that 80 characters. Would you not agree? ";
+		Lexer lexer = new Lexer(input);
+		TestUtils.assertToken(lexer.getToken(), expected, Sym.T_STR_LITERAL);
+	}
 
-  @Test
-  public void testGetTokenStringTooLong() {
-    Lexer lexer = new Lexer(
-        "'This test string is much, much longer that 80 characters. Would you not agree? I would.';");
-    token = lexer.getToken();
-    assertEquals(token.GetType(), Sym.T_STR_LITERAL);
-    assertEquals(
-        token.GetLexeme(),
-        "This test string is much, much longer that 80 characters. Would you not agree? ");
-  }
+	@Test
+	public void testGetTokenStringEOF() {
+		String input = "'This test string is much, much longer that 80 characters. Would you not agree? I would. EOF;";
+		String expected = "This test string is much, much longer that 80 characters. Would you not agree? ";
+		Lexer lexer = new Lexer(input);
+		TestUtils.assertToken(lexer.getToken(), expected, Sym.T_STR_LITERAL);
+	}
 
-  @Test
-  public void testGetTokenStringEOF() {
-    Lexer lexer = new Lexer(
-        "'This test string is much, much longer that 80 characters. Would you not agree? I would. EOF;");
-    token = lexer.getToken();
-    assertEquals(token.GetType(), Sym.T_STR_LITERAL);
-    assertEquals(
-        token.GetLexeme(),
-        "This test string is much, much longer that 80 characters. Would you not agree? ");
-  }
+	@Test
+	public void testGetTokenStringNewLine() {
+		String input = "'This test string is much, much longer that 80 characters. Would you not agree? I would.\n;";
+		String expected = "This test string is much, much longer that 80 characters. Would you not agree? ";
+		Lexer lexer = new Lexer(input);
+		TestUtils.assertToken(lexer.getToken(), expected, Sym.T_STR_LITERAL);
+	}
 
-  @Test
-  public void testGetTokenStringNewLine() {
-    Lexer lexer = new Lexer(
-        "'This test string is much, much longer that 80 characters. Would you not agree? I would.\n;");
-    token = lexer.getToken();
-    assertEquals(token.GetType(), Sym.T_STR_LITERAL);
-    assertEquals(
-        token.GetLexeme(),
-        "This test string is much, much longer that 80 characters. Would you not agree? ");
-  }
+	@Test
+	public void testGetTokenStringDoubleSingle() {
+		String input = "\"test 'in single quotes.'\";";
+		String expected = "test 'in single quotes.'";
+		Lexer lexer = new Lexer(input);
+		TestUtils.assertToken(lexer.getToken(), expected, Sym.T_STR_LITERAL);
+	}
 
-  @Test
-  public void testGetTokenStringDoubleSingle() {
-    Lexer lexer = new Lexer("\"test 'in single quotes.'\";");
-    token = lexer.getToken();
-    assertEquals(token.GetType(), Sym.T_STR_LITERAL);
-    assertEquals(token.GetLexeme(), "test 'in single quotes.'");
-  }
-
-  @Test
-  public void testGetTokenStringSingleDouble() {
-    Lexer lexer = new Lexer("'test \"in double quotes.\"';");
-    token = lexer.getToken();
-    assertEquals(token.GetType(), Sym.T_STR_LITERAL);
-    assertEquals(token.GetLexeme(), "test \"in double quotes.\"");
-  }
+	@Test
+	public void testGetTokenStringSingleDouble() {
+		String input = "'test \"in double quotes.\"';";
+		String expected = "test \"in double quotes.\"";
+		Lexer lexer = new Lexer(input);
+		TestUtils.assertToken(lexer.getToken(), expected, Sym.T_STR_LITERAL);
+	}
 
 }
